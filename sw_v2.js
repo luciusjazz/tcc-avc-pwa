eb1e6a9a3f6679d83dad90a9ef9d7bd3ab82da32
luciusjazz <luciusjazz@gmail.com>
2025-07-30 14:51:39 -0300

    Add files via upload

---
+++ sw_v2.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'avc-check-cache-v2'; // Nome do cache atualizado
+const urlsToCache = [
+  '/',
+  '/index.html'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cache v2 aberto');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
